440a0d09c0a4471992600ca6779f44b178781627
Neeraj <neeraj@skybin.io>
2018-03-28 23:53:56 +0530

    .

---
+++ COREAPI/DATA/NUAGEDbContext.cs
@@ -0,0 +1,25 @@
+﻿using COREAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COREAPI.DATA
+{
+    public class NUAGEDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+
+        public NUAGEDbContext(DbContextOptions<NUAGEDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ COREAPI/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COREAPI.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public int? UserType { get; set; }
+    }
+
+}
